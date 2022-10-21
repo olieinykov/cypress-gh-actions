0e23bd6d3a0575577db3f964b3377091dc365c07
AntonOlieinykov <olieinykov@gmail.com>
2022-10-21 16:37:22 +0300

    Print logs

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null
+        }
+      })
       // implement node event listeners here
     },
   },
--- cypress/e2e/my-test.cy.js
+++ cypress/e2e/my-test.cy.js
@@ -1,5 +1,6 @@
 describe('empty spec', () => {
   it('passes', () => {
+    cy.task('log', Cypress.env('TEST_VARIABLE_EXAMPLE'))
     cy.visit('/');
     cy.get('[data-cy="variable-value"]').contains(Cypress.env('TEST_VARIABLE_EXAMPLE'))
   })
